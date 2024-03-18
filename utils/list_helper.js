868b461bef3b76d126c3a4b676e046353b529deb
Matthew Di Micelli <matthewdimicelli@gmail.com>
2024-03-17 21:27:34 -0400

    - complete 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -8,6 +8,26 @@ const totalLikes = (arrOfBlogPosts) => {
   }, 0);
 }
 
+const favoriteBlog = (arrOfBlogs) => {
+  if (arrOfBlogs.length === 0) {
+    return undefined;
+  }
+
+  let mostLikes = 0;
+  let mostLikedBlog;
+  for (let blog of arrOfBlogs) {
+    if (blog.likes > mostLikes) {
+      mostLikes = blog.likes;
+      mostLikedBlog = blog;
+    }
+  }
+  return {
+    title: mostLikedBlog.title,
+    author: mostLikedBlog.author,
+    likes: mostLikedBlog.likes,
+  }
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
