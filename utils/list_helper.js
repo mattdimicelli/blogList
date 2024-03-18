8de9a5b6b9e38b3a44db4693c849b861d48e4a92
Matthew Di Micelli <matthewdimicelli@gmail.com>
2024-03-17 21:13:22 -0400

    - complete 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
   return 1;
 }
 
+const totalLikes = (arrOfBlogPosts) => {
+  return arrOfBlogPosts.reduce((total, post) => {
+    return total + post.likes;
+  }, 0);
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
