86ea0265e899eb87bfc7b66cb1b0243adc73efed
Matthew Di Micelli <matthewdimicelli@gmail.com>
2024-03-29 20:15:12 -0400

    - complete 4.15

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const blogsRouter = require('./controllers/blogs')
+const userRouter = require('./controllers/users')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const { mongoDbUri } = require('./utils/config')
@@ -11,5 +12,6 @@ app.use(cors())
 app.use(express.json())
 
 app.use(blogsRouter)
+app.use(userRouter)
 
 module.exports = app
--- controllers/users.js
+++ controllers/users.js
@@ -2,15 +2,27 @@ const userRouter = require('express').Router()
 const User = require('../models/user')
 const bcrypt = require('bcrypt')
 
-userRouter.post('/', async (req, res) => {
+userRouter.post('/api/users', async (req, res) => {
   try {
     const { username, password, name } = req.body
     const saltRounds = 10
     const hashedPassword = await bcrypt.hash(password, saltRounds)
-    const user = new User({ username, hashedPassword, name })
+    const user = new User({ username, password: hashedPassword, name })
     const newUser = await user.save()
+    res.status(201).json(newUser)
   } catch (e) {
     console.error(e)
+    res.status(400).send({ error: 'invalid request' })
+  }
+})
+
+userRouter.get('/api/users', async(req, res) => {
+  try {
+    const allUsers = await User.find({})
+    res.json(allUsers);
+  } catch(e) {
+    console.error(e)
+    res.status(400).send({ error: 'invalid request' })
   }
 })
 
